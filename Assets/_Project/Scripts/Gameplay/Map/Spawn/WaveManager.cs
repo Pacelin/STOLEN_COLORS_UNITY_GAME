148b93d82f4df0d984138905d7f2d01d2c67c486
Pacelin <Warfacer2020@gmail.com>
2024-10-07 13:40:19 +0300

    feat: Wave Manager With State

--- Assets/_Project/Scripts/Gameplay/Map/Spawn/WaveManager.cs
+++ Assets/_Project/Scripts/Gameplay/Map/Spawn/WaveManager.cs
@@ -1,20 +1,27 @@
-﻿namespace Gameplay.Map.Spawn
+﻿using UniRx;
+
+namespace Gameplay.Map.Spawn
 {
     public class WaveManager
     {
+        public IReadOnlyReactiveProperty<bool> WaveIsInProgress => _waveIsInProgress;
+
         private readonly WarriorsSpawner _warriors;
         private readonly CastlesCollection _castles;
-        
+        private readonly ReactiveProperty<bool> _waveIsInProgress;
+
         public WaveManager(WarriorsSpawner warriors, CastlesCollection castles)
         {
             _warriors = warriors;
             _castles = castles;
+            _waveIsInProgress = new(false);
         }
 
         public void StartWave()
         {
             _castles.ReleaseCastles();
             _warriors.SpawnEnemiesWave();
+            _waveIsInProgress.Value = true;
         }
 
         public void CaptureCastle(Castle castle, Warrior warrior)
@@ -23,6 +30,7 @@
             _castles.SetCapturingCastle(castle);
             _castles.SnapCastles();
             _warriors.StopWave();
+            _waveIsInProgress.Value = false;
         }
     }
 }
