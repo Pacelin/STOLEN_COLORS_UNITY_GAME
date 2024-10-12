54100ca918effa86ebc1057570126a8d1e381bd5
uniowlik <777ruspacan777@gmail.com>
2024-10-12 17:54:31 +0300

    wip camera controller rework + stacktrace

--- Assets/_Project/Scripts/CameraController.cs
+++ Assets/_Project/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 public class CameraController : MonoBehaviour
@@ -22,25 +23,33 @@ public class CameraController : MonoBehaviour
     private Transform _start, _goal;
     
     [SerializeField]
-    private float _initialMoveSpeed = 10f, _cameraGoalStopDelay = 3f, _lerpSpeed = 1f;
+    private float _goalMoveTime = 3f;
+
+    [SerializeField]
+    private float _cameraGoalStopDelay = 3f;
 
     private Tween _tween;
 
     [SerializeField]
-    private Vector3 _cameraOffset = new(0f, 10f, 0f), _castleOffset = new(5f, 0f, 0f);
+    private Vector3 _cameraOffset = new(0f, 10f, 0f), _castleOffset = new(5f, 0f, 0f), _creatureOffset = new(5f, 0f, 0f);
 
-    private bool _update = false;
-    private Vector3 _lastPos;
+    [SerializeField]
+    private bool _showLevelGoal;
     
-    private Func<Vector3> _getCameraTargetPosition;
-
+    private Vector3 _initialPosition;
+    private Transform _currentTarget;
+    private Vector3 _currentTargetPosition;
+    private Vector3 _targetOffset;
+    private float _moveTime;
+    
     private CompositeDisposable _disposables = new();
     
     private void OnEnable()
     {
-        _waveManager.WaveIsInProgress.Subscribe(value =>
-            _getCameraTargetPosition = value ? GetMostRightAllyPosition : GetAllyCastlePosition).AddTo(_disposables);
-        ShowLevelGoal();
+        _waveManager.WaveIsInProgress.Subscribe(UpdateCurrentTarget).AddTo(_disposables);
+        
+        if (_showLevelGoal)
+            ShowLevelGoal();
     }
 
     private void OnDisable()
@@ -48,74 +57,117 @@ public class CameraController : MonoBehaviour
         _disposables.Dispose();
     }
 
-    public void ShowLevelGoal()
-    {
-        var startPosition = _start.position;
-        startPosition.y = 0;
-        startPosition.z = 0;
-
-        var goalPosition = _goal.position;
-        goalPosition.y = 0;
-        goalPosition.z = 0;
-        
-        float distance = Mathf.Abs(startPosition.x - goalPosition.x);
-        float time = distance / _initialMoveSpeed;
-
-        _tween?.Kill();
-        _camera.position = startPosition + _cameraOffset;
-
-        var startPos = _getCameraTargetPosition();
-        startPos.z = 0;
-        startPos.y = 0;
-        
-        _tween = DOTween.Sequence().
-                Append(_camera.DOMove(goalPosition + _cameraOffset, time)).
-                AppendInterval(_cameraGoalStopDelay).
-                Append(_camera.DOMove(startPos + _cameraOffset, time)).
-                SetEase(Ease.InOutFlash).
-                AppendCallback(() => _update = true);
-    }
-
     private void LateUpdate()
     {
-        if (!_update)
-            return;
-        
         MoveCamera();
     }
 
+    private void ShowLevelGoal()
+    {
+        _tween?.Kill();
+        _tween = DOTween.Sequence().
+                AppendCallback(() => SetTarget(_goal, _goalMoveTime)).
+                AppendInterval(_goalMoveTime + _cameraGoalStopDelay).
+                AppendCallback(() => SetTarget(_start, _goalMoveTime));
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        if (_currentTarget)
+            return _currentTarget.position + _targetOffset;
+        return _currentTargetPosition + _targetOffset;
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _initialPosition = transform.position;
+        _currentTarget = target;
+    }
+    
+    public void SetTarget(Vector3 targetPosition)
+    {
+        _initialPosition = transform.position;
+        _currentTarget = null;
+        _currentTargetPosition = targetPosition;
+    }
+    
+    public void SetTarget(Transform target, float time)
+    {
+        _initialPosition = transform.position;
+        
+        _currentTarget = target;
+        
+        _moveTime = time;
+    }
+
+    public void SetTarget(Vector3 targetPosition, float time)
+    {
+        _initialPosition = transform.position;
+        
+        _currentTarget = null;
+        _currentTargetPosition = targetPosition;
+        
+        _moveTime = time;
+    }
+
+    public void SetTargetOffset(Vector3 targetOffset)
+    {
+        _targetOffset = targetOffset;
+    }
+    
     private void MoveCamera()
     {
-        var allyPosition = _getCameraTargetPosition();
-        var targetPosition = allyPosition + _cameraOffset;
-        targetPosition.x = Mathf.Clamp(targetPosition.x, 
-            _start.position.x + _cameraOffset.x, 
-            _goal.position.x + _cameraOffset.x);
+        Debug.Log(_currentTarget);
+        
+        var targetPosition = GetTargetPosition();
+        var currentPosition = Vector3.Lerp(_initialPosition, targetPosition, Time.deltaTime / _moveTime);
 
-        var cameraPosition = _camera.position;
-        cameraPosition.x = Mathf.Lerp(cameraPosition.x, targetPosition.x, Time.deltaTime * _lerpSpeed);
-
-        _camera.position = cameraPosition;
+        Debug.Log(Vector3.Lerp(_initialPosition, targetPosition, Time.deltaTime / _moveTime));
+        _camera.position = currentPosition + _cameraOffset;
     }
 
-    private Vector3 GetAllyCastlePosition()
+    private void UpdateCurrentTarget(bool waveStart)
+    {
+        if (waveStart)
+            SetTargetAllyCreature();
+        else
+            SetTargetCastle();
+    }
+
+    private void SetTargetCastle()
     {
         var castle = _castles.GetCurrentCastle(EBattleSide.Ally);
-        _lastPos = (castle ? castle.transform.position : _start.position) + _castleOffset;
-        return _lastPos;
-    }
 
-    private Vector3 GetMostRightAllyPosition()
+        if (castle)
+        {
+            SetTarget(castle.transform);
+            SetTargetOffset(_castleOffset);
+        }
+        else
+        {
+            SetTarget(_start);
+            SetTargetOffset(Vector3.zero);
+        }
+    }
+    
+    private void SetTargetAllyCreature()
     {
         var warrior = _warriors.Allies.OrderByDescending(w => w.transform.position.x).FirstOrDefault();
         if (warrior)
-            _lastPos = warrior.transform.position;
-        else
         {
-            var enemy = _warriors.Enemies.OrderBy(w => w.transform.position.x).FirstOrDefault();
-            if (enemy)
-                _lastPos = enemy.transform.position;
+            SetTarget(warrior.transform);
+            SetTargetOffset(_creatureOffset);
+            _targetOffset = _creatureOffset;
+            return;
         }
-        return _lastPos;
+
+        var enemy = _warriors.Enemies.OrderBy(w => w.transform.position.x).FirstOrDefault();
+        if (enemy)
+        {
+            SetTarget(enemy.transform);
+            SetTargetOffset(-_creatureOffset);
+            return;
+        }
+        SetTargetCastle();
     }
 }
---
+++ Assets/_Project/Scripts/Tutorial/TutorialPanels.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TutorialPanels : MonoBehaviour
+{
+    [SerializeField]
+    private TutorialEntry[] _entries;
+
+    private void Start()
+    {
+        
+    }
+
+    public void NextEntry()
+    {
+        
+    }
+}
