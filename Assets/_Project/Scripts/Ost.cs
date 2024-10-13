eb8046c9d1e4c72f89183b3a65adf11273025bbd
Pacelin <Warfacer2020@gmail.com>
2024-10-13 03:27:37 +0300

    feat: Software Cursor

--- Assets/_Project/Scripts/Ost.cs
+++ Assets/_Project/Scripts/Ost.cs
@@ -6,10 +6,12 @@ namespace Audio
     public class Ost : MonoBehaviour
     {
         [SerializeField] private EMusicKey _ost;
+        [SerializeField] private Texture2D _cursor;
         [Inject] private AudioSystem _audio;
 
         private void Awake()
         {
+            Cursor.SetCursor(_cursor, new Vector2(3, 0), CursorMode.ForceSoftware);
             Time.timeScale = 1f;
             _audio.PlayMusic(_ost, true).Forget();
         }
